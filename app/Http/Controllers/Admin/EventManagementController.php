625311203fd8dfd45e6e7de3a7de3dbb61f516e1
Mohammad Naim Khan <mohammadnaimkhan66@gmail.com>
2024-02-06 18:34:21 +0600

    update

--- app/Http/Controllers/Admin/EventManagementController.php
+++ app/Http/Controllers/Admin/EventManagementController.php
@@ -71,10 +71,51 @@ class EventManagementController extends Controller
     }
 
 
+    public function update(Request $request, Event $event)
+    {
+
+        $data = $request->validate([
+            'name'                => 'required|unique:events,name',
+            'district_id'         => 'required',
+            'desc'                => 'required',
+            'location'            => 'required',
+            'date'                => 'required',
+            'image_caption'       => 'required',
+        ]);
+
+        try {
+
+            if ($request->hasFile('image')) {
+
+                // delete previous image
+                ImageHelper::removeImage($event->image, '/uploads/event/');
+
+                $request->validate([
+                    'image' => 'required|file|max:2000|mimes:png,jpg,jpeg,gif,svg'
+                ]);
+
+                $fileName = ImageHelper::image($request->image, '/uploads/event/');
+
+                $data['image'] = $fileName;
+            }
+
+            $event->update($data);
+
+            toastr()->success('Updated Successfully');
+
+            return redirect()->route('admin.event.index');
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+
+
 
     public function destroy(Event $event)
     {
         try {
+            // delete image
             ImageHelper::removeImage($event->image, '/uploads/event/');
 
             $event->delete();
